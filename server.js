2a44f47311406663a465584805653c7caf8339f3
Elan Zinkow <ezinkow@gmail.com>
2021-03-08 21:13:58 -0600

    fixing server for heroku

--- server.js
+++ server.js
@@ -3,29 +3,46 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 // const db = require('./models');
-const environment = process.env.NODE_ENV || 'development';
+// const environment = process.env.NODE_ENV || 'development';
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 console.log(process.env.NODE_ENV);
-if (environment === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 // Add routes, both API and view
 app.use(routes);
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
-  {
+if (process.env.NODE_ENV === "production") {
+  mongoose.connect(process.env.MONGODB_URI,  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }
-);
+  });
+} else {
+  mongoose.connect("mongodb+srv://admin:Password1@cluster0.zrvnu.mongodb.net/googlebooks?retryWrites=true&w=majority",  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+}
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// );
 
 // Start the API server
 
